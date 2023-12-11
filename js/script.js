6ddc9ac7a55a169a28a84f4a845d06dfd36dddf9
erenerdogan98 <eren.erdogannn@hotmail.com>
2023-12-11 18:25:45 +0300

    Henüz tamamlanmadı ama son değişiklikler ile arayüz bu şekilde

--- js/script.js
+++ js/script.js
@@ -2,19 +2,39 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
 // scroll sections
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+    sections.forEach(s => {
+        let top = window.scrollY
+        let offset = s.offsetTop - 100
+        let height = s.offsetHeight
+        let id = s.getAttribute('id')
 
+        if ( top >= offset && top < offset + height) {
+            // active navbar link
+            navLinks.forEach(links => {
+                links.classList.remove('active')
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            })
+        }
+    })
 
     // sticky header
+    let header = document.querySelector('header')
 
+    header.classList.toggle('sticky',window.scrollY > 100)
 
     // remove toggle icon and navbar when click navbar links (scroll)
-
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active')
 
     // animation footer on scroll
 
